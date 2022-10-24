0048521122033072e623f718f8ac487099868892
JontePonte <john_nordstrand@hotmail.com>
2022-10-24 19:07:40 +0200

    Data is collected from sign up form and messages are displayed

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from website import create_app
 
 app = create_app()
+DEBUG_MODE = True
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if DEBUG_MODE:
+        app.run(debug=True)
+    else:
+        app.run(debug=False)
--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
@@ -14,5 +16,22 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def signUp():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash("Email must be greater than 4 characters.", category='error')
+        elif len(first_name) < 2:
+            flash("First name must be greater than 1 character.", category='error')
+        elif password1 != password2:
+            flash("Passwords don't match.", category='error')
+        elif len(password1) < 7:
+            flash("Password must have at least 7 characters", category='error')
+        else:
+            flash("Account created!", category='success')
+
     return render_template("sign_up.html")
 
