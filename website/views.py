64531f291463518bf256c1db1019426062a9e4fb
JontePonte <john_nordstrand@hotmail.com>
2022-10-26 22:07:29 +0200

    Added delete function for notes

--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+import json
 from flask import Blueprint, render_template, flash, request
 from flask_login import login_required, current_user
 from .models import Note
@@ -21,3 +22,16 @@ def home():
             flash('Note added', category='success')
 
     return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    data = json.loads(request.data)
+    noteId = data['noteId']
+    note = Note.query.get(noteId)
+
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return json.jsonify({})
